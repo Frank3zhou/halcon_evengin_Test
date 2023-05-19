9db71ddb485015e23b52823f77d13f371517efa0
FRANK <1750326565@qq.com>
2023-05-19 17:47:20 +0800

    “2023/5/19”

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace halcon引擎测试
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(232, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 651);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "载入图片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "载入halcon文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(26, 620);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(185, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "运行";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1044, 675);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using HalconDotNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace halcon引擎测试
+{
+    public partial class Form1 : Form
+    {
+        ZKHwindows M_ZKHwindows = new ZKHwindows();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            M_ZKHwindows.Dock = DockStyle.Fill; 
+            panel1.Controls.Add(M_ZKHwindows);  
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string imgpath = choose_file();
+            HObject img;
+            HOperatorSet.ReadImage(out img, imgpath);
+            M_ZKHwindows.NowImage = img;
+        }
+        public string choose_file()
+        {
+            try
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+                // 设置对话框的标题和筛选条件
+                openFileDialog1.Title = "选择图像文件";
+                openFileDialog1.Filter = "图像文件 (*.bmp, *.jpg, *.jpeg, *.png) | *.bmp; *.jpg; *.jpeg; *.png";
+
+                // 打开文件对话框并检查用户是否选择了文件
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    // 获取所选文件的完整路径
+                    string selectedImagePath = openFileDialog1.FileName;
+
+                    // 获取所选文件的名称
+                    return selectedImagePath;
+
+                    // 在控制台输出所选文件的名称
+                    // Console.WriteLine("所选文件的名称是: " + selectedImageName);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+
+        public string choose_Halcon_file()
+        {
+            try
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+                // 设置对话框的标题和筛选条件
+                openFileDialog1.Title = "选择halcon文件";
+                openFileDialog1.Filter = "halcon文件 (*.hdvp, *.hdev) | *.hdvp; *.hdev; ";
+
+                // 打开文件对话框并检查用户是否选择了文件
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    // 获取所选文件的完整路径
+                    string selectedImagePath = openFileDialog1.FileName;
+
+                    // 获取所选文件的名称
+                    return selectedImagePath;
+
+                    // 在控制台输出所选文件的名称
+                    // Console.WriteLine("所选文件的名称是: " + selectedImageName);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           string  halconfilepath = choose_Halcon_file();
+           try
+           {
+               if (halconfilepath.Contains(".hdev"))
+               {
+                     string   hdevpath = halconfilepath;
+                     HDevProgram  hprogram = new HDevProgram(hdevpath);
+                     HDevProgramCall  hprocall = new HDevProgramCall(hprogram);
+                     hprocall.Execute();
+                    //获得hdev程序里SelectedRegions1变量内容并显示
+                    HObject region = hprocall.GetIconicVarRegion("Regions");
+                   HOperatorSet.DispObj(region, M_ZKHwindows.hWindowControl.HalconWindow);
+                 //  M_ZKHwindows.hWindowControl.HalconWindow.DispObj(region);
+                   //执行hdev程序并获取参数
+                  
+                //   var files = hprocall.GetCtrlVarTuple("ImageFiles");
+
+               }
+               else if (halconfilepath.Contains(".hdvp"))
+               {
+
+               }
+               else
+               {
+                   MessageBox.Show("崽种，这可不是我要的");
+               }
+            }
+           catch (Exception exception)
+           {
+               MessageBox.Show("崽种，这可不是我要的");
+            }
+         
+        }
+    }
+}
